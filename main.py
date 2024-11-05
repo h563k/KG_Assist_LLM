81ababf74672de27810d80277ae784c6135dddc0
Your Name <you@example.com>
2024-11-05 18:03:08 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from functional.llm_api import llm_local
 from functional.setting import ModelConfig
-from functional.llm_promot import semantic_analysis
+from functional.llm_promot import mbti_analysis
 
 if __name__ == '__main__':
-    config = ModelConfig()
-    model_name = config.ollama['model_name']
-    for mbti_type, txt in config.mbti_data.values:
-        result = semantic_analysis(txt, model_name)
-        print(result)
-        break
+    mbti_analysis()
 
     # system_prompt = '请用中文回答问题'
     # prompt = '你知道 mbti 吗'
