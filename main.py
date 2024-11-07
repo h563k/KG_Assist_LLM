677d2be8652aec71ccc6d8f2b7bda66c9281c4f4
Your Name <you@example.com>
2024-11-07 18:14:39 +0000

    Initial commit

--- main.py
+++ main.py
@@ -6,8 +6,4 @@ if __name__ == '__main__':
     # mbti_analysis(model_name='qwen2.5:72b-instruct-q4_0')
     result = mbti_analysis(lens=10)
     print(result)
-    # system_prompt = '请用中文回答问题'
-    # prompt = '你知道 mbti 吗'
-    # model_name = 'llama3.1:70b'
-    # stream = False
-    # print(llm_local(system_prompt, prompt, model_name, stream))
+
