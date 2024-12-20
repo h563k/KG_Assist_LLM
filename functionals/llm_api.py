1a25d7b36a2a11a4d3e81cb7cfdc09bb9ed8897a
Your Name <you@example.com>
2024-12-20 05:10:56 +0000

    push

--- functionals/llm_api.py
+++ functionals/llm_api.py
@@ -3,58 +3,17 @@ import random
 import time
 from functionals.system_config import ModelConfig
 from functionals.standard_log import log_to_file
-
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)  # for exponential backoff
 
 config = ModelConfig()
 mbti = config.mbti
-# define a retry decorator
 
 
-def retry_with_exponential_backoff(
-    func,
-    initial_delay: float = 1,
-    exponential_base: float = 2,
-    jitter: bool = True,
-    max_retries: int = 10,
-    errors: tuple = (openai.error.RateLimitError,),
-):
-    """Retry a function with exponential backoff."""
-
-    def wrapper(*args, **kwargs):
-        # Initialize variables
-        num_retries = 0
-        delay = initial_delay
-
-        # Loop until a successful response or max_retries is hit or an exception is raised
-        while True:
-            try:
-                return func(*args, **kwargs)
-
-            # Retry on specific errors
-            except errors as e:
-                # Increment retries
-                num_retries += 1
-
-                # Check if max retries has been reached
-                if num_retries > max_retries:
-                    raise Exception(
-                        f"Maximum number of retries ({max_retries}) exceeded."
-                    )
-
-                # Increment the delay
-                delay *= exponential_base * (1 + jitter * random.random())
-
-                # Sleep for the delay
-                time.sleep(delay)
-
-            # Raise exceptions for any errors not specified
-            except Exception as e:
-                raise e
-
-    return wrapper
-
-
-@retry_with_exponential_backoff
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
 @log_to_file
 def openai_response(system_prompt, prompt, openai_type='openai_origin', stream=True) -> str:
     openai.api_key = config.OpenAI[openai_type]['api_key']
