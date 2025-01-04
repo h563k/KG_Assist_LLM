d07d62e75777ba81faa39783022251b636652594
Your Name <you@example.com>
2025-01-04 18:26:03 +0000

    push

--- functionals/data_clean.py
+++ functionals/data_clean.py
@@ -2,10 +2,10 @@ import re
 from functionals.llm_api import bert_api
 
 
-def data_process(txt: str, deepclean=True, cutoff=3500):
+def data_process(task: str, deepclean=True, cutoff=3500):
     temp = []
-    txt = txt.split('|||')
-    for message in txt:
+    task = task.split('|||')
+    for message in task:
         website = re.findall('(https://\S+|http://\S+)', message)
         if not website:
             temp.append(message)
@@ -17,22 +17,22 @@ def data_process(txt: str, deepclean=True, cutoff=3500):
     if not deepclean:
         return txt
     # bert筛选，先进行句子拆分
-    temp1 = []
+    sentences = []
     for contents in txt.split('\n'):
         bodys = contents.split('. ')
         for body in bodys:
             if len(body.split(' ')) <= 5:
                 continue
-            temp1.extend(bodys)
+            sentences.extend(bodys)
     # 按长度截断
     count = 0
-    temp2 = []
-    for i, message in enumerate(temp1):
+    bert_process = []
+    for  message in sentences:
         is_mbti = bert_api(message)
         if is_mbti:
-            temp2.append(message)
+            bert_process.append(message)
             count += len(message)
         if count > cutoff:
             break
-    txt = "\n".join(temp2)
+    txt = "\n".join(bert_process)
     return txt
