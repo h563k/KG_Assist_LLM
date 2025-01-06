9c415b0df7b02e438908377ef29215adb650501b
Your Name <you@example.com>
2025-01-06 18:20:35 +0000

    push

--- functionals/analysis.py
+++ functionals/analysis.py
@@ -7,17 +7,22 @@ from functionals.standard_log import log_to_file
 
 @log_to_file
 def mbti_analysis(start, end, dataset='kaggle'):
-    config = ModelConfig()
-    data = config.mbti_data[dataset]
-    mbti = MbtiChats()
-    result = []
-    for i, (mbti_real, task) in enumerate(data.values[start:end]):
-        mbti.run(task)
-        result.append([i, mbti_real, mbti.chat_result['final_mbti']])
-    count = np.zeros(4)
-    for _, mbti_real, mbti_predict in result:
-        for i in range(4):
-            if mbti_real[i] == mbti_predict[i]:
-                count[i] += 1
-    count /= len(result)
-    return [f"{x*100:.4f}%" for x in count], stats.hmean(count), len(result)
+    try:
+        config = ModelConfig()
+        data = config.mbti_data[dataset]
+        mbti = MbtiChats()
+        result = [] 
+        count = 0
+        for i, (mbti_real, task) in enumerate(data.values[start:end]):
+            count += 1
+            mbti.run(task)
+            result.append([i, mbti_real, mbti.chat_result['final_mbti']])
+        count = np.zeros(4)
+        for _, mbti_real, mbti_predict in result:
+            for i in range(4):
+                if mbti_real[i] == mbti_predict[i]:
+                    count[i] += 1
+        count /= len(result)
+        return [f"{x*100:.4f}%" for x in count], stats.hmean(count), len(result)
+    except Exception as e:
+        return result, count, e
