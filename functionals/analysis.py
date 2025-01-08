c4cfd13ada6f7c029025129f36f6f8a5fef6cf19
Your Name <you@example.com>
2025-01-08 02:23:20 +0000

    push

--- functionals/analysis.py
+++ functionals/analysis.py
@@ -1,3 +1,5 @@
+import os
+import json
 import numpy as np
 from scipy import stats
 from functionals.agent import MbtiChats
@@ -5,13 +7,29 @@ from functionals.system_config import ModelConfig
 from functionals.standard_log import log_to_file, debug
 
 
+def get_start_end():
+    _, _, file_name = os.walk(
+        '/opt/project/KG_Assist_LLM/logs/debug').__next__()
+    file_name = [name.split('_') for name in file_name]
+    file_name.sort(key=lambda x: x[1])
+    file_name = file_name[-1]
+    start = int(file_name[1])
+    file_name = "_".join(file_name)
+    with open(f"/opt/project/KG_Assist_LLM/logs/debug/{file_name}", 'r') as f:
+        data = json.load(f)
+    for num in data:
+        start = max(start, (num[0]))
+    start = start + 1
+
+
 @log_to_file
-def mbti_analysis(start, end, dataset='kaggle'):
+def mbti_analysis(end, dataset='kaggle'):
     result = []
     config = ModelConfig()
     data = config.mbti_data(dataset)
     mbti = MbtiChats()
     try:
+        start = get_start_end()
         for i, (mbti_real, task) in enumerate(data.values[start:end]):
             print("origin_task")
             print(task)
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from functionals.analysis import mbti_analysis
 
 
 if __name__ == '__main__':
-    mbti_analysis(start=49, end=3002, dataset='kaggle')
+    mbti_analysis(end=3002, dataset='kaggle')
