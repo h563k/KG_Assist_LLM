4cc2b9f575a3808be84f34ff7e7a975b32efd9b7
Your Name <you@example.com>
2025-01-10 18:32:00 +0000

    push

--- functionals/agent.py
+++ functionals/agent.py
@@ -63,7 +63,7 @@ class MbtiChats:
             }
         ]
         llm_config = {"config_list": config_list,
-                      "cache_seed": None}
+                      "cache_seed": openai_config['cache_seed']}
         return llm_config
 
     @staticmethod
