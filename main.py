7f70ad07e1c61594718355d74256d0f9e428d6ef
Your Name <you@example.com>
2025-01-12 01:54:45 +0000

    push

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from functionals.analysis import mbti_analysis
 
 
 if __name__ == '__main__':
-    mbti_analysis(start=0, end=350, dataset='kaggle')
+    mbti_analysis(start=0, end=350, dataset='pand')
